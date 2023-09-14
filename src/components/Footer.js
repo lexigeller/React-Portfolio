ce433e35f951e8919321d9d2b67c9a908d1e6e3f
Lexi Geller <leximoconner@yahoo.com>
2023-09-14 14:35:26 -0400

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { VscGithubAlt } from "react-icons/vsc";
+import { AiOutlineLinkedin } from "react-icons/ai";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <p>&copy; 2023 Lexi Geller</p>
+          </div>
+          <div className="col-12 col-md-6">
+            <ul className="social-icons">
+              <li className="logo">
+                <a href="https://github.com/lexigeller/" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-github"></i>
+                  <VscGithubAlt />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/lexi-m-geller/" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-linkedin"></i>
+                  <AiOutlineLinkedin />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
