88edf4142fadef900a75514b7d080e35f8d900c7
Lexi Geller <leximoconner@yahoo.com>
2023-09-14 14:49:19 -0400

    split Header and Navigation into separate files

---
+++ src/components/Navigation.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+function Navigation() {
+  const [activeNavItem, setActiveNavItem] = useState('About Me');
+
+  const handleNavItemClick = (item) => {
+    setActiveNavItem(item);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">Lexi Geller | Portfolio</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className={`nav-item ${activeNavItem === 'About Me' ? 'active' : ''}`}>
+              <a
+                className="nav-link"
+                href="#about-me"
+                onClick={() => handleNavItemClick('About Me')}
+              >
+                About Me
+              </a>
+            </li>
+            <li className={`nav-item ${activeNavItem === 'Portfolio' ? 'active' : ''}`}>
+              <a
+                className="nav-link"
+                href="#portfolio"
+                onClick={() => handleNavItemClick('Portfolio')}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className={`nav-item ${activeNavItem === 'Contact' ? 'active' : ''}`}>
+              <a
+                className="nav-link"
+                href="#contact"
+                onClick={() => handleNavItemClick('Contact')}
+              >
+                Contact
+              </a>
+            </li>
+            <li className={`nav-item ${activeNavItem === 'Resume' ? 'active' : ''}`}>
+              <a
+                className="nav-link"
+                href="#resume"
+                onClick={() => handleNavItemClick('Resume')}
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
