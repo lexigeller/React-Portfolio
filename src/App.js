3c829148b4895dad063d798b87a35853a99394bf
Lexi Geller <leximoconner@yahoo.com>
2023-09-14 16:16:09 -0400

    fixed issue in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
