fce7e76bf36732e8c18dba40f6ca3c3b6796172c
Lexi Geller <leximoconner@yahoo.com>
2023-09-14 16:01:25 -0400

    added App.js code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import AboutMe from './AboutMe';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Switch>
+            <Route path="/" exact component={AboutMe} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/resume" component={Resume} />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
