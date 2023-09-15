52a73b3055ba8b796ee5af58908cda9d829f918d
Lexi Geller <leximoconner@yahoo.com>
2023-09-15 16:49:52 -0400

    further nav styling, headshot styling, tweaking Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,57 +11,44 @@ function Navigation() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
         <a className="navbar-brand">Portfolio | Lexi Geller</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className={`nav-item ${activeNavItem === 'About Me' ? 'active' : ''}`}>
-              <a
-                className="nav-link"
-                href="#about-me"
-                onClick={() => handleNavItemClick('About Me')}
-              >
-                About Me
-              </a>
-            </li>
-            <li className={`nav-item ${activeNavItem === 'Portfolio' ? 'active' : ''}`}>
-              <a
-                className="nav-link"
-                href="#portfolio"
-                onClick={() => handleNavItemClick('Portfolio')}
-              >
-                Portfolio
-              </a>
-            </li>
-            <li className={`nav-item ${activeNavItem === 'Contact' ? 'active' : ''}`}>
-              <a
-                className="nav-link"
-                href="#contact"
-                onClick={() => handleNavItemClick('Contact')}
-              >
-                Contact
-              </a>
-            </li>
-            <li className={`nav-item ${activeNavItem === 'Resume' ? 'active' : ''}`}>
-              <a
-                className="nav-link"
-                href="#resume"
-                onClick={() => handleNavItemClick('Resume')}
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-nav">
+          <li className={`nav-item ${activeNavItem === 'About Me' ? 'active' : ''}`}>
+            <a
+              className="nav-link"
+              href="#about-me"
+              onClick={() => handleNavItemClick('About Me')}
+            >
+              About Me
+            </a>
+          </li>
+          <li className={`nav-item ${activeNavItem === 'Portfolio' ? 'active' : ''}`}>
+            <a
+              className="nav-link"
+              href="#portfolio"
+              onClick={() => handleNavItemClick('Portfolio')}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className={`nav-item ${activeNavItem === 'Contact' ? 'active' : ''}`}>
+            <a
+              className="nav-link"
+              href="#contact"
+              onClick={() => handleNavItemClick('Contact')}
+            >
+              Contact
+            </a>
+          </li>
+          <li className={`nav-item ${activeNavItem === 'Resume' ? 'active' : ''}`}>
+            <a
+              className="nav-link"
+              href="#resume"
+              onClick={() => handleNavItemClick('Resume')}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
