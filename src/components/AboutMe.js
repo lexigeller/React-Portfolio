defbcb866dc7d6f71e83d413090324fcb484ac6f
Lexi Geller <leximoconner@yahoo.com>
2023-09-16 12:36:56 -0400

    fixed about me styling, footer bullet point

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,24 +5,24 @@ import headshot from '../assets/headshot.jpg';
 function AboutMe() {
   return (
     <section id="about-me" className="about-me">
-      <div class="container" className="headshot-container">
+      <div className="headshot-container">
         <img src={headshot} alt="Headshot" className="headshot" />
         <div className="headshot-overlay"></div>
       </div>
-      <div>
-        <h1 id="about">
-          About Me
-        </h1>
-        <p>
-          Hello! I'm Lexi Geller, a recent web development graduate.
-        </p>
-        <p>
-          My long-held interest in patterns and languages led me to the University of Florida, where I obtained a Bachelor's degree in Linguistics with a minor in East Asian Literatures, Languages, and Cultures (Chinese). That, combined with an interest in technology, led me to web development. I recently graduated a full-stack web development bootcamp at the University of Central Florida and am eager to put my newly-honed skills to use.
-        </p>
-        <p>
-          Let's build together.
-        </p>
-      </div>        
+      <div className="about-me-content">
+        <h1 id="about">About Me</h1>
+        <div className="container">
+          <p>
+            Hello! I'm Lexi Geller, a recent web development graduate.
+          </p>
+          <p>
+            My long-held interest in patterns and languages led me to the University of Florida, where I obtained a Bachelor's degree in Linguistics with a minor in East Asian Literatures, Languages, and Cultures (Chinese). That, combined with an interest in technology, led me to web development. I recently graduated a full-stack web development bootcamp at the University of Central Florida and am eager to put my newly-honed skills to use.
+          </p>
+          <p>
+            Let's build together.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,6 +24,12 @@ function Footer() {
                   <i className="fab fa-linkedin"></i>
                   <AiOutlineLinkedin />
                 </a>
+                <style>{`
+                  /* Custom CSS to remove the bullet point */
+                  .social-icons li.logo {
+                    list-style: none;
+                  }
+                `}</style>
               </li>
             </ul>
           </div>
