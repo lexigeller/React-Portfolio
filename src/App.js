a8bbfad520ae53a1b906600c3094094fe3133242
Lexi Geller <leximoconner@yahoo.com>
2023-09-16 00:06:38 -0400

    tweaked code to keep header and footer on each page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Navigation from './components/Navigation';
-import AboutMe from './components/AboutMe'; 
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
@@ -8,21 +8,32 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  const [activeSection, setActiveSection] = useState('About Me');
+  const [activeSection, setActiveSection] = useState('About Me'); // Set the default active section
 
   const handleNavItemClick = (section) => {
     setActiveSection(section);
   };
 
+  // Render the corresponding component based on the activeSection
+  const renderSection = () => {
+    switch (activeSection) {
+      case 'About Me':
+        return <AboutMe />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <AboutMe />; // Default to About Me if no match
+    }
+  };
+
   return (
     <div className="App">
-      <Navigation activeSection={activeSection} onNavItemClick={handleNavItemClick} />
-      <main>
-        {activeSection === 'About Me' && <AboutMe />}
-        {activeSection === 'Portfolio' && <Portfolio />}
-        {activeSection === 'Contact' && <Contact />}
-        {activeSection === 'Resume' && <Resume />}
-      </main>
+      <Header onNavItemClick={handleNavItemClick} activeSection={activeSection} />
+      <main>{renderSection()}</main>
       <Footer />
     </div>
   );
