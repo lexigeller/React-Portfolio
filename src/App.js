c1c4c5cad80d746dd7c20ea5f0f07ca5d4109b3f
Lexi Geller <leximoconner@yahoo.com>
2023-09-15 23:17:36 -0400

    more debugging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import Header from './components/Header';
-import AboutMe from './components/AboutMe';
+import Navigation from './components/Navigation';
+import AboutMe from './components/AboutMe'; 
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
-import './App.css';
 
 function App() {
   const [activeSection, setActiveSection] = useState('About Me');
@@ -16,12 +15,12 @@ function App() {
 
   return (
     <div className="App">
-      <Header onNavItemClick={handleNavItemClick} activeSection={activeSection} />
+      <Navigation activeSection={activeSection} onNavItemClick={handleNavItemClick} />
       <main>
-        {window.location.pathname === '/about' && <AboutMe />}
-        {window.location.pathname === '/portfolio' && <Portfolio />}
-        {window.location.pathname === '/contact' && <Contact />}
-        {window.location.pathname === '/resume' && <Resume />}
+        {activeSection === 'About Me' && <AboutMe />}
+        {activeSection === 'Portfolio' && <Portfolio />}
+        {activeSection === 'Contact' && <Contact />}
+        {activeSection === 'Resume' && <Resume />}
       </main>
       <Footer />
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,54 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Navigation() {
-  const [activeNavItem, setActiveNavItem] = useState('About Me');
-
-  const handleNavItemClick = (item) => {
-    setActiveNavItem(item);
-  };
-
+function Navigation({ activeSection, onNavItemClick }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">Portfolio | Lexi Geller
+      <div className="container">
+        Portfolio | Lexi Geller
         <a className="navbar-brand"></a>
         <ul className="navbar-nav">
-            <li className={`nav-item ${activeNavItem === 'About Me' ? 'active' : ''}`}>
-                <a
-                className="nav-link"
-                href="/about"  
-                onClick={() => handleNavItemClick('About Me')}
-                >
-                    About Me
-                </a>
-            </li>
-            <li className={`nav-item ${activeNavItem === 'Portfolio' ? 'active' : ''}`}>
-                <a
-                className="nav-link"
-                href="/portfolio"
-                onClick={() => handleNavItemClick('Portfolio')}
-                >
-                    Portfolio
-                </a>
-                </li>
-  <li className={`nav-item ${activeNavItem === 'Contact' ? 'active' : ''}`}>
-    <a
-      className="nav-link"
-      href="/contact"  // Link to your Contact page
-      onClick={() => handleNavItemClick('Contact')}
-    >
-      Contact
-    </a>
-  </li>
-  <li className={`nav-item ${activeNavItem === 'Resume' ? 'active' : ''}`}>
-    <a
-      className="nav-link"
-      href="/resume"  // Link to your Resume page
-      onClick={() => handleNavItemClick('Resume')}
-    >
-      Resume
-    </a>
-  </li>
-</ul>
+          <li className={`nav-item ${activeSection === 'About Me' ? 'active' : ''}`}>
+            <a
+              className="nav-link"
+              href="#about-me"
+              onClick={() => onNavItemClick('About Me')}
+            >
+              About Me
+            </a>
+          </li>
+          <li className={`nav-item ${activeSection === 'Portfolio' ? 'active' : ''}`}>
+            <a
+              className="nav-link"
+              href="#portfolio"
+              onClick={() => onNavItemClick('Portfolio')}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className={`nav-item ${activeSection === 'Contact' ? 'active' : ''}`}>
+            <a
+              className="nav-link"
+              href="#contact"
+              onClick={() => onNavItemClick('Contact')}
+            >
+              Contact
+            </a>
+          </li>
+          <li className={`nav-item ${activeSection === 'Resume' ? 'active' : ''}`}>
+            <a
+              className="nav-link"
+              href="#resume"
+              onClick={() => onNavItemClick('Resume')}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
