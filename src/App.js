86b76d745c8ca5e83c67b703f2e2ec1e9c6f6e87
Lexi Geller <leximoconner@yahoo.com>
2023-09-16 11:20:28 -0400

    readding header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Header from './components/Header';
-import AboutMe from './components/AboutMe';
+import Navigation from './components/Navigation';
+import AboutMe from './components/AboutMe'; 
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
@@ -8,32 +8,22 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  const [activeSection, setActiveSection] = useState('About Me'); // Set the default active section
+  const [activeSection, setActiveSection] = useState('About Me');
 
   const handleNavItemClick = (section) => {
     setActiveSection(section);
   };
 
-  // Render the corresponding component based on the activeSection
-  const renderSection = () => {
-    switch (activeSection) {
-      case 'About Me':
-        return <AboutMe />;
-      case 'Portfolio':
-        return <Portfolio />;
-      case 'Contact':
-        return <Contact />;
-      case 'Resume':
-        return <Resume />;
-      default:
-        return <AboutMe />; // Default to About Me if no match
-    }
-  };
-
   return (
     <div className="App">
-      <Header onNavItemClick={handleNavItemClick} activeSection={activeSection} />
-      <main>{renderSection()}</main>
+      <Header />
+      <Navigation activeSection={activeSection} onNavItemClick={handleNavItemClick} />
+      <main>
+        {activeSection === 'About Me' && <AboutMe />}
+        {activeSection === 'Portfolio' && <Portfolio />}
+        {activeSection === 'Contact' && <Contact />}
+        {activeSection === 'Resume' && <Resume />}
+      </main>
       <Footer />
     </div>
   );
